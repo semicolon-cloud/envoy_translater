8065653b9a65b8fcbfb27cadbffb9077763043dc
syeyoung <cyoung06@naver.com>
2024-04-17 17:16:55 +0900

    - update file

---
+++ envoy_translator/commands/management/commands/exampleconfig.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+import confspirator
+
+from envoy_translator import config
+
+
+class Command(BaseCommand):
+    help = "Produce an example config file for Adjutant."
+
+    def add_arguments(self, parser):
+        parser.add_argument("--output-file", default="adjutant.yaml")
+
+    def handle(self, *args, **options):
+        print("Generating example file to: '%s'" % options["output_file"])
+
+        confspirator.create_example_config(config._root_config, options["output_file"])
